1b0a091a5e327b3d2ca9bfd86719e6e59146ba64
Sara Naeem <63316988+sara-naeem-cs@users.noreply.github.com>
2024-11-23 22:15:19 -0500

    Included videoes that can be used for testing as well as the code for the display

--- src/emotions.py
+++ src/emotions.py
@@ -12,7 +12,10 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
+import face_recognition
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -46,6 +49,29 @@ def plot_model_history(model_history):
     fig.savefig('plot.png')
     plt.show()
 
+def create_emotion_graph(title, values):
+  categories = ['Angry', 'Disgusted', 'Fearful', 'Happy', 'Neutral', 'Sad', 'Surprised']
+  colours = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'pink']
+  px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+  fig, ax = plt.subplots(figsize=(9, 4), dpi=200) #width, height
+  ax.bar(categories, values, color=colours)
+  ax.set_xlabel('Emotions', fontsize=18)
+  ax.set_ylabel('Probability of Emotion in Current Frame (for current frame)', fontsize=18)
+  ax.set_title('Emotion Probability: ' + title, fontsize=18)
+  ax.tick_params(axis='x', labelsize=15)  
+  ax.tick_params(axis='y', labelsize=14)  
+  return fig
+
+def image_padding(image):
+  top_padding = (3000-image.shape[0]) // 2
+  bottom_padding = 3000 - top_padding - image.shape[0]
+  image = cv2.copyMakeBorder(image, top_padding, bottom_padding, 0, 0, cv2.BORDER_CONSTANT, None, value = 0) 
+  return image
+
+def graph_padding(graph):
+  image = cv2.copyMakeBorder(graph, 0, (3000-(graph.shape[0])), 0, 0, cv2.BORDER_CONSTANT, None, value = 0) 
+  return image
+
 # Define data generators
 train_dir = 'data/train'
 val_dir = 'data/test'
@@ -53,7 +79,7 @@ val_dir = 'data/test'
 num_train = 28709 + 436 #Adding 436 to account for newly added images to disgust folder. 
 num_val = 7178
 batch_size = 64
-num_epoch = 5 #changed from 50 to 5
+num_epoch = 40 #changed from 50 to 5
 
 #Preparing images for training:
 train_datagen = ImageDataGenerator(rescale=1./255)
@@ -106,12 +132,16 @@ if mode == "train":
     plot_model_history(model_info)
     model.save_weights('model.weights.h5')
 
+    loss, accuracy = model.evaluate(train_generator, steps=num_val // batch_size)
+    print(f"Test Loss: {loss}")
+    print(f"Test Accuracy: {accuracy}")
 
+
+    """
     train_accuracy = model_info.history['accuracy']
     val_accuracy = model_info.history['val_accuracy']
     print("The train accuracy is " + str(train_accuracy))
     print("The test accuracy is " + str(val_accuracy))
-
 
     #Creating confusion matrix:
     y_pred = model.predict(validation_generator)#Predicted values
@@ -122,8 +152,24 @@ if mode == "train":
     sns.heatmap(cm/np.sum(cm), annot=True, 
             fmt='.2%', cmap='Blues', xticklabels=class_labels, yticklabels=class_labels)
     plt.show()
+    """
 
+elif mode == "test":
+    model.compile(loss='categorical_crossentropy',optimizer=Adam(learning_rate=0.0001, decay=1e-6),metrics=['accuracy'])
+    model.load_weights('model.weights.h5')
+    testing_generator = val_datagen.flow_from_directory(
+            val_dir,
+            target_size=(48,48),
+            batch_size=batch_size,
+            color_mode="grayscale",
+            class_mode='categorical')
 
+    num_test = 42
+    batch_size = 32
+    # Evaluate the model on the validation set
+    loss, accuracy = model.evaluate(testing_generator, steps=num_test // batch_size)
+    print(f"Test Loss: {loss}")
+    print(f"Test Accuracy: {accuracy}")
 
 
 
@@ -165,6 +211,111 @@ elif mode == "display":
     cv2.destroyAllWindows()
     """
     print("Webcam would have deployed, commented out temporarily.")
+    i = 0 
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    #Video application features from here:
+    cap = cv2.VideoCapture("videos/sad.mp4")
+    face_locations = []
+    process_this_frame = True
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = cap.read()
+
+        #Get current height and width of the image
+        height = frame.shape[0]
+        width = frame.shape[1]
+        print(frame.shape)
+        #Resize frame to 2/3 of the height and width to increase processing speed while still retaining quality
+        frame = cv2.resize(frame, (int(width*2/3), int(height*2/3)))
+
+        if process_this_frame:
+            """
+            # Find all the faces in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_frame)
+            for top, right, bottom, left in face_locations:
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            """
+
+            # Load the Haar Cascade for face detection
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+            # Frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Find all faces
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+            
+            #Counter to keep track of the number of faces in the frame
+            counter = 0 
+            #returned_graphs will take emotions graph for each face and append them.
+            result_graphs = None
+
+            # For each face detected, draw a rectangle and get the emotion breakdown for it.
+            for (x, y, w, h) in faces:
+                if counter < 3:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    cv2.putText(frame, "Face" + str(counter+1), (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    
+                    #Co-ordinates of the selected rectangle
+                    roi_gray = gray[y:y + h, x:x + w] 
+                    #Resize the rectangle
+                    cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                    #Make a prediction (will return the probability breakdown of each emotion)
+                    prediction = model.predict(cropped_img)
+                    #Convert prediciton to numpy array 
+                    prediction = np.array(prediction)
+                    #print(prediction) #ex.[[7.2752759e-01, 2.3053539e-05, 5.0827023e-04, 9.4853419e-07, 9.6426476e-05,9.9926704e-01, 9.7008124e-05]]
+
+                    #Used for writing out the breakdown of emotions in legible format
+                    emotion_breakdown = ""
+                    for j in range(len(prediction[0])):
+                        emotion_breakdown = emotion_breakdown + " " + emotion_dict[j] + ": " + str(prediction[0][j])
+                    #Returns emotion and corresponding percentage:
+                    print(emotion_breakdown) 
+                    #ex.  Angry: 8.481411e-09 Disgusted: 0.00028628838 Fearful: 1.0516198e-05 Happy: 0.94973195 Neutral: 9.047412e-09 Sad: 0.04997055 Surprised: 6.466511e-07
+
+                    #We plot the returned emotion prediction
+                    fig = create_emotion_graph("Face " + str(counter+1), prediction[0])
+                    fig.canvas.draw()
+
+                    #Convert the plot to an image (code taken from URL included below)
+                    plot = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8,sep='')
+                    plot = plot.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+                    plot = cv2.cvtColor(plot, cv2.COLOR_RGB2BGR) #Convert to RGB for matplotlib
+
+
+                    if counter < 1:
+                        result_graphs = plot
+                    elif counter >= 1:
+                        #Append both graphs one on top of the other. 
+                        result_graphs = np.vstack([result_graphs, plot])
+                    counter = counter + 1
+                
+            print(str(counter))
+            plot = graph_padding(result_graphs)
+            #plot = image_padding(result_graphs)
+            frame = image_padding(frame)
+            result_img = np.hstack([frame, plot])
+            print(result_img.shape) #Height, width, 3000 x 3040
+        
+        process_this_frame = not process_this_frame #Process every other frame (due to resource constraints)
+        
+
+        # Display the resulting image
+        cv2.imshow("Image", result_img)
+
+        #Printing out which frame we're on:
+        print("This is frame: ", i)
+        i = i + 1
+        
+        # Wait for 'q'' key to stop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 
 
@@ -172,3 +323,15 @@ elif mode == "display":
 #Resources:
 # https://stackoverflow.com/questions/53351963/mnist-get-confusion-matrix 
 # https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea
+#https://stackoverflow.com/questions/58887056/resize-frame-of-cv2-videocapture
+#https://github.com/ageitgey/face_recognition/issues/1336
+#https://note.nkmk.me/en/python-opencv-pillow-image-size/ #Getting height, width of image
+#https://pyimagesearch.com/2021/01/20/opencv-resize-image-cv2-resize/ #Resizing an image
+# https://matplotlib.org/stable/gallery/subplots_axes_and_figures/figure_size_units.html #Adjusting figure height in pixels
+# https://www.geeksforgeeks.org/python-opencv-cv2-copymakeborder-method/ #For adding padding
+# https://stackoverflow.com/questions/6390393/how-to-change-tick-label-font-size
+
+#References for videos used:
+#Video by Mikhail Nilov: https://www.pexels.com/video/a-couple-looking-at-a-smartphone-screen-6963479/
+#Video by cottonbro studio: https://www.pexels.com/video/an-excited-young-female-at-game-arcade-5767473/ 
+# Video by fauxels: https://www.pexels.com/video/close-up-video-of-man-wearing-red-hoodie-3249935/ 
